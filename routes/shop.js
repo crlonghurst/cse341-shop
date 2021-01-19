688f7a3ee3b1efada017ea80ede89e4cc322aae5
crlonghurst <christianrlonghurst@gmail.com>
2021-01-19 13:15:00 -0700

    moved where shop app is

---
+++ routes/shop.js
@@ -0,0 +1,8 @@
+const path = require('paht');
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/products')
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
