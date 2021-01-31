dd34d416b2b225af58c3f1984bfad91c6bd540b1
crlonghurst <christianrlonghurst@gmail.com>
2021-01-30 18:27:15 -0700

    Week 4 prove

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,10 +18,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('6015ba65fd54b55a741dc9eb')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+mongoose
+  .connect(
+    'mongodb+srv://clonghurst:cse341Shop@cluster0.6siqs.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'User',
+          email: 'user@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
